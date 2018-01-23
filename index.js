2e6c33d6d5ea564bd07f34b8127d976130ebe2b6
JserShadow <424108968@qq.com>
2018-01-23 20:15:54 +0800

    stable

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const insert = require('./mongoInsert.js');
+
+insert();
+
+// var server = app.listen(3000, function () {
+//     var host = server.address().address;
+//     var port = server.address().port;
+//     console.log('Server running at', host, port);
+// });
